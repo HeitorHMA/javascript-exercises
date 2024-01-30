4ad187f1230360c237e3ae09fa757bd953b8a4e6
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-01-30 12:25:43 -0300

    Learn

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, x) {
 
+    const index = myArray.indexOf(x);
+    
+    const i = myArray.splice(index, 1);
+    return myArray
 };
 
 // Do not edit below this line
